3b5d8f0447bd56b8df4ed6fd7328d0063883a12e
Sepehr Saeedpour <sepsad@Sepehrs-MacBook-Air.local>
2021-08-29 22:15:15 +0430

    connection messages are addded

--- server/server.js
+++ server/server.js
@@ -1,14 +1,30 @@
 const path = require("path");
-const publicPath = path.join(__dirname, "..", "public");
-
-const port = process.env.PORT || 3000
-
+const http = require("http");
 const express = require("express");
-app = express();
+const socketIO = require("socket.io");
+
+
+const publicPath = path.join(__dirname, "..", "public");
+const port = process.env.PORT || 3000;
+
+let app = express();
+let server = http.createServer(app);
+let io = socketIO(server);
+
+io.on('connection', (socket) => {
+    console.log("NEW USER IS HERE!")
+    
+    socket.on("disconnect", () => {
+        console.log("new user is disconnected from server.")
+
+    })
+
+});
+
 
 
 app.use(express.static(publicPath))
 
-app.listen(port, () => {
+server.listen(port, () => {
     console.log(`Server is up on port ${port}`)
 })
