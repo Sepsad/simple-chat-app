54e185e497c5962e494171c173e45cd42b21a2ee
Sepehr Saeedpour <sepsad@Sepehrs-MacBook-Air.local>
2021-08-30 15:29:05 +0430

    server and clinet codes have written.

--- public/js/index.js
+++ public/js/index.js
@@ -1,7 +1,11 @@
 const chatForm = document.getElementById('chat-form');
-const chatMessage = document.getElementById()
+const chatMessage = document.querySelector('.chat-messages');
+const roomName = document.getElementById('room-name');
+const userList = document.getElementById('users');
 
-
+const {username, room} = Qs.parse(location.search, {
+    ignoreQueryPrefix: true,
+});
 
 let socket = io();
 
@@ -12,3 +16,82 @@ socket.on("connect", function () {
 socket.on("disconnect", function () {
     console.log("you are disConnected!")
 } )
+
+socket.emit('joinRoom', {username, room});
+
+socket.on('roomUsers', ({room, users}) => {
+    showRoomName(room);
+    showUsers(users);
+});
+
+// Message from server
+socket.on('messaage', (message) => {
+    console.log(message);
+    showMessage(message);
+
+    chatMessage.scrollTop = chatMessage.scrollHeight;
+});
+
+chatForm.addEventListener('submit', (e) => {
+    console.log(e);
+
+    let msg = e.target.elements.msg.value;
+
+    console.log(msg);
+
+    msg = msg.trim();
+
+    if(!msg) {
+
+        return false
+    }
+
+    socket.emit('message', msg);
+
+    e.target.elements.msg.value = '';
+    e.target.elements.msg.focus();
+});
+
+
+
+
+
+
+// show message to DOM
+function showMessage(message) {
+    const div = document.createElement('div');
+    div.classList.add('message');
+
+    const p = document.createElement('p');
+    p.classList.add('meta');
+    p.innerText = message.username;
+
+    div.appendChild(p);
+
+    const p_message = document.createElement('p');
+    p_message.classList.add('text');
+    p_message.innerText = message.text;
+
+    div.appendChild(p_message)
+
+    document.querySelector('.chat-messages').appendChild(div);
+};
+
+// show room name
+
+function showRoomName(room) {
+
+    roomName.innerHTML = room;
+}
+
+//show users
+
+function showUsers(users) {
+    userList.innerHTML = '';
+    users.forEach((user) => {
+        const li = document.createElement('li');
+        li.innerText = user.username;
+        userList.appendChild(li);
+    });
+}
+
--- server/server.js
+++ server/server.js
@@ -2,32 +2,69 @@ const path = require("path");
 const http = require("http");
 const express = require("express");
 const socketIO = require("socket.io");
+const formatMessage = require("./utils/message");
 
+const { 
+    userJoin,
+    getCurrentUser,
+    userLeave,
+    getRoomUsers} = require("./utils/users")
 
-const publicPath = path.join(__dirname, "..", "public");
-const port = process.env.PORT || 3000;
 
 let app = express();
 let server = http.createServer(app);
 let io = socketIO(server);
 
-io.on('connection', (socket) => {
-    console.log("NEW USER IS HERE!")
+// static folder
+const publicPath = path.join(__dirname, "..", "public");
+app.use(express.static(publicPath))
 
-    socket.on( 'creatMessage', (messeage) => {
-        console.log("creatMessage", messeage);
-    } )
+// run when user connects
+io.on('connection', (socket) => {
+
+    console.log("NEW USER IS Conncted!")
+
+    socket.on( "joinRoom", ({username, room}) => {
+        console.log(`Joined! ${username} to ${room}`)
+
+        const user = userJoin(socket.id, username, room);
+
+        socket.join(user.room);
+
+        socket.emit("message", formatMessage("Admin", `WELCOME! ${username}`));
+
+        socket.broadcast.to(user.room).emit('message',
+                                            formatMessage('Admin', `${username} has joined!`));
+        io.to(user.room).emit('roomUsers', {
+            room: user.room,
+            users: getRoomUsers(user.room)
+        });
+    });
+
+    socket.on('chatMessage', (msg) => {
+        const user = getCurrentUser(socket.id);
+
+        io.to(user.room).emit('message', formatMessage(user.username, msg));
+    });
     
     socket.on("disconnect", () => {
-        console.log("new user is disconnected from server.")
+        console.log("user is disconnected from server.")
 
-    })
+        const user = userLeave(socket.id);
 
+        if (user) {
+            io.to(user.room).emit('message', formatMessage('Admin',`${user.username} has left!`));
+
+            io.to(user.room).emit('roomUsers', {
+                room: user.room,
+                users: getRoomUsers(user.room)
+            });
+        }
+    });
 });
 
 
-
-app.use(express.static(publicPath))
+const port = process.env.PORT || 3000;
 
 server.listen(port, () => {
     console.log(`Server is up on port ${port}`)
